8df3810d25c7768defa9648cafcf1a3d5ae3bc25
justinwoolley@gmail.com <jwoolley@netflix.com>
2021-04-02 00:13:14 -0700

    random color hexes

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class HexMapGenerator : MonoBehaviour {
     [SerializeField]
@@ -17,15 +18,52 @@ public class HexMapGenerator : MonoBehaviour {
         GenerateHexMap();
     }
 
+    enum TileColor {
+        RED,
+        BLUE,
+        GREEN,
+        PURPLE,
+        ORANGE,
+        YELLOW
+    };
+
+    Material getNewMaterialWithColor(Color color) {
+        MeshRenderer meshRenderer = HexTilePrefab.GetComponent<MeshRenderer>();
+        Material material = Instantiate(meshRenderer.sharedMaterial);
+        material.SetColor("_Color", color);
+        return material;
+    }
+
+    Material getRandomHexColorMaterial() {
+        TileColor tileColorEnum = (TileColor)Random.Range(0, 6);
+        Debug.Log($"Setting hex color to {tileColorEnum}");
+        return referenceHexes[tileColorEnum];
+    }
+
+    Dictionary<TileColor, Material> referenceHexes;
+
+    void intializeReferenceHexMap() {
+        referenceHexes = new Dictionary<TileColor, Material>();
+        referenceHexes.Add(TileColor.RED, getNewMaterialWithColor(Color.red));
+        referenceHexes.Add(TileColor.BLUE, getNewMaterialWithColor(Color.blue));
+        referenceHexes.Add(TileColor.GREEN, getNewMaterialWithColor(Color.green));
+        referenceHexes.Add(TileColor.PURPLE, getNewMaterialWithColor(new Color(0.6f, 0.4f, 1.0f, 1.0f)));
+        referenceHexes.Add(TileColor.ORANGE, getNewMaterialWithColor(new Color(1.0f, 0.6f, 0.2f, 1.0f)));
+        referenceHexes.Add(TileColor.YELLOW, getNewMaterialWithColor(Color.yellow));
+    }
+
     void GenerateHexMap() {
         float initialOffsetX = (1 - mapHexWidth) / 2.0f;
         float initialOffsetY = (1 - mapHexHeight) / 2.0f;
+
+        intializeReferenceHexMap();
 
         for (int i = 0; i < mapHexHeight; i++) {
             float y = i + initialOffsetY;
             for (int j = 0; j < mapHexWidth; j++) {
                 float x = j + initialOffsetX;
                 GameObject tempGameObject = Instantiate(HexTilePrefab);
+                tempGameObject.GetComponent<MeshRenderer>().sharedMaterial = getRandomHexColorMaterial();
 
                 if (i % 2 == 0) {
                     tempGameObject.transform.position = new Vector3(x * hexTileOffset_X, y * hexTileOffset_Y, 0);
